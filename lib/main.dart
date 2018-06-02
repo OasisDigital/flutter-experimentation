3197a6e10cb0b95c041944fe68d659cf1a9c613a
Luke McNeil <lukemcneil61@gmail.com>
2018-06-01 20:22:29 -0500

    first project

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp(new Worker('joe', 300)));
+}
+
+class MyApp extends StatefulWidget {
+  final Worker worker;
+
+  MyApp(this.worker);
+
+  @override
+  createState() {
+    return new MyAppState(worker);
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  Worker worker;
+
+  MyAppState(this.worker);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Directionality(
+        child: new Text(
+            '\n\n\nmy name is ${worker.employee} and I have worked ${365-worker.daysWorked} days'),
+        textDirection: TextDirection.ltr);
+  }
+}
+
+class Worker {
+  String employee;
+  int daysWorked;
+
+  Worker(this.employee, this.daysWorked);
+}
