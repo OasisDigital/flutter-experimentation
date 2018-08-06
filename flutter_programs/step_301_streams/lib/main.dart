d681a8b12956134316152f219ad970b6117e2a14
Unknown <lukemcneil61@gmail.com>
2018-08-06 15:08:23 -0500

    renamed 302 to 301

--- flutter_programs/step_301_streams/lib/main.dart
+++ flutter_programs/step_301_streams/lib/main.dart
@@ -5,9 +5,12 @@ import 'dart:async';
 
 void main() => runApp(new MyApp());
 
+int numberOfEmployees = 5;
+
 Stream employeesStream() async* {
   List data = await getEmployees();
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < numberOfEmployees; i++) {
+    await Future.delayed(Duration(milliseconds: 250));
     yield '${data[i]['first_name']}' + ' ' + '${data[i]['last_name']}';
   }
 }
@@ -29,24 +32,24 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStreamBuilder extends StatelessWidget {
-  final Stream employees = employeesStream().asBroadcastStream().take(5);
-
-  void makeWidgets(List list) {
-    employees.forEach((employee) {
-      print(employee);
-      list.add(
-        Card(
-          child: ListTile(
-            title: Text(employee),
+  final Stream employees = employeesStream().asBroadcastStream().take(numberOfEmployees);
+  void makeWidgetsList(List list) {
+    employees.forEach(
+      (employee) {
+        list.add(
+          Card(
+            child: ListTile(
+              title: Text(employee),
+            ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
 
   Widget build(BuildContext context) {
-    List<Widget> widgets = List();
-    makeWidgets(widgets);
+    List<Widget> widgetsList = List();
+    makeWidgetsList(widgetsList);
 
     return Scaffold(
       appBar: AppBar(
@@ -57,7 +60,12 @@ class MyStreamBuilder extends StatelessWidget {
         stream: employees,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView(children: widgets);
+            return ListView.builder(
+              itemCount: widgetsList.length,
+              itemBuilder: (context, i) {
+                return widgetsList[i];
+              },
+            );
           }
           return Center(child: CircularProgressIndicator());
         },
